5f62f5b37400ea4193980f8b359c22b0854f1075
GiovannApolinar <gnapolinar1@up.edu.ph>
2025-05-02 19:16:10 +0800

    chore: (temporary) update home in Material App to LoginPage

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:planago/navigation_menu.dart';
+// import 'package:planago/navigation_menu.dart';
+import 'package:planago/screens/login_page.dart';
 import 'package:planago/utils/theme/theme.dart';
 
 class App extends StatelessWidget {
@@ -10,7 +11,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.defaultTheme,
-      home: const NavigationMenu(),
+      home: const LoginPage(),
     );
   }
 }
