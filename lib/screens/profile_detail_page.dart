b5b3dd0cfe2d8e06d96de358777cc31307c9c1a2
ALRnoche <arnoche1@up.edu.ph>
2025-05-05 00:19:48 +0800

    fix(detail_page): fixed design

--- lib/screens/profile_detail_page.dart
+++ lib/screens/profile_detail_page.dart
@@ -1,23 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:planago/screens/profile_page.dart';
+import 'package:planago/utils/constants/colors.dart'; // Assuming you have an AppColors class
+import 'package:planago/screens/profile_page.dart'; // Assuming ProfileInfo is from this file
 
-class ProfileDetailPage extends StatelessWidget 
+class ProfileDetailPage extends StatefulWidget 
 {
   final ProfileInfo item;
 
   const ProfileDetailPage({super.key, required this.item});
 
   @override
+  _ProfileDetailPageState createState() => _ProfileDetailPageState();
+}
+
+
+
+class _ProfileDetailPageState extends State<ProfileDetailPage> 
+{
+  Widget profilePicture(double width, double height) 
+  {
+    return SizedBox(
+      width: width * 0.88,
+      height: height * 0.1728, //142
+      child: Column(
+        children: [
+          SizedBox.square(
+            dimension: height * 0.1169, //102
+            child: ClipOval(
+              child: Image.asset(
+                'assets/images/default_profile.png',
+              ), // temp only
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) 
   {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(title: Text('${item.title} Details')),
+      appBar: AppBar(
+        title: Text(
+          '${widget.item.title} Details',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: screenHeight * 0.0252,
+            fontWeight: FontWeight.w600,
+            color: AppColors.secondary,
+          ),
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('${item.title}: ${item.subtitle}', style: TextStyle(fontSize: 20)),
+            profilePicture(screenWidth, screenHeight),
+            const SizedBox(height: 20),
+            Text('${widget.item.title}: ${widget.item.subtitle}',
+                style: TextStyle(fontSize: 20)),
             const SizedBox(height: 10),
           ],
         ),
