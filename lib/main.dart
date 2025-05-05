1c688326eb9412c00cfd960865b0a7aa7f15149b
Myko Javier <mmjavier5@up.edu.ph>
2025-05-05 14:51:14 +0800

    setup: initialized firebase connection in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:planago/app.dart';
 import 'package:planago/controllers/signup_controller.dart';
+import 'package:planago/firebase_options.dart';
 
-  void main() {
+  Future<void> main() async {
   Get.put<SignupController>(SignupController());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
